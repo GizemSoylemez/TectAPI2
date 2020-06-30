91e106155afcb525775958f292414bc03314372e
GizemSoylemez <37050175+GizemSoylemez@users.noreply.github.com>
2020-06-30 09:42:20 +0300

    correction

--- ATest/Models/Book.cs
+++ ATest/Models/Book.cs
@@ -7,7 +7,7 @@ namespace ATest.Models
 {
     public class Book
     {
-        //public static readonly Id;
+       
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
